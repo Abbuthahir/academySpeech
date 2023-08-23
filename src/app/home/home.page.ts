ee569e1e39c5b476186ec7e77b20fdddac4c8054
Abbuthahir <abbuthahir04@gmail.com>
2023-08-23 17:43:44 +0530

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { TextToSpeech } from '@capacitor-community/text-to-speech';
+import { SpeechRecognition } from '@capacitor-community/speech-recognition';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  myText = 'Today is a good day to learn Ionic';
+  recording = false;
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+    SpeechRecognition.requestPermissions();
+  }
 
-  constructor() {}
+  async startRecognition() {
+    const { available } = await SpeechRecognition.available();
 
+    if (available) {
+      this.recording = true;
+      SpeechRecognition.start({
+        popup: false,
+        partialResults: true,
+        language: 'en-US'
+      });
+      SpeechRecognition.addListener('partialResults', (data: any) => {
+        console.log('partialResults was fired', data.matches);
+        if (data.matches && data.matches.length > 0) {
+          this.myText = data.matches[0];
+          this.changeDetectorRef.detectChanges();
+        }
+        if (data.value && data.value.length > 0) {
+          this.myText = data.value[0];
+          this.changeDetectorRef.detectChanges();
+        }
+      })
+    }
+  }
+
+  async stopRecognition() {
+    this.recording = false;
+    await SpeechRecognition.stop();
+  }
+
+
+  async speakText() {
+    if (this.myText.trim() !== '') {
+      TextToSpeech.speak({
+        text: this.myText,
+      });
+    }
+  }
 }
